486ee855fdfd06fc6be7072f50e2df374c01c377
Tristan Bagot <tristan.bagot@gmail.com>
2018-11-12 14:12:35 +0100

    ok

--- www/site/plugins/intanong.php
+++ www/site/plugins/intanong.php
@@ -1,7 +1,7 @@
 <?php
 
 page::$methods['displayCredits'] = function($page, $limit = null) {
-    
+
     $credits = $page->credits()->toStructure();
     if($limit) $credits->limit($limit);
 
@@ -9,8 +9,8 @@ page::$methods['displayCredits'] = function($page, $limit = null) {
 
     foreach ($credits as $key => $c) {
     	if ($c->title()->isNotEmpty()) {
-			if($c->link()->isNotEmpty()) {
-				$html .= html::tag('a', $c->title()->lower().': '.$c->text(), ['class' => 'credit', 'href' => $c->link()]);
+			if($c->url()->isNotEmpty()) {
+				$html .= html::tag('a', $c->title()->lower().': '.$c->text(), ['class' => 'credit', 'href' => $c->url()]);
 			} else {
 				$html .= html::tag('div', $c->title()->lower().': '.$c->text(), ['class' => 'credit']);
 			}
